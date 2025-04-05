cdb1e71447e746ad7282fe4e632ee0f2de5983d0
claradonato <mclaralimadonato@gmail.com>
2025-04-05 16:02:37 -0300

    primeiro commit da atividade

---
+++ Q02_IfElse.js
@@ -0,0 +1,9 @@
+function verificarIntervalo(num) {
+    if(num >= 7 && num <= 10) {
+        console.log('No Intervalo');
+    } else {
+        console.log('Não está no intervalo');
+    }
+}
+
+verificarIntervalo(7);
